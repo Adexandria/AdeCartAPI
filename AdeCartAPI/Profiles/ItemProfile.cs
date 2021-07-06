9b5c56d5a457d92807f12394ef53fa975e097c04
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-06 17:46:59 +0100

    Create Item profile

---
+++ AdeCartAPI/Profiles/ItemProfile.cs
@@ -0,0 +1,32 @@
+﻿using AdeCartAPI.DTO;
+using AdeCartAPI.Model;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdeCartAPI.Profiles
+{
+    public class ItemProfile:Profile
+    {
+        public ItemProfile()
+        {
+            CreateMap<Item, ItemDTO>()
+            .ForMember(s => s.Name, opt => opt.MapFrom(s => s.ItemName))
+            .ForMember(s => s.Price, opt => opt.MapFrom(s => s.ItemPrice))
+            .ForMember(s => s.Description, opt => opt.MapFrom(s => s.ItemDescription));
+
+            CreateMap<ItemCreate, Item>()
+           .ForMember(s => s.ItemName, opt => opt.MapFrom(s => s.Name))
+           .ForMember(s => s.ItemPrice, opt => opt.MapFrom(s => s.Price))
+           .ForMember(s => s.ItemDescription, opt => opt.MapFrom(s => s.Description));
+
+            CreateMap<ItemUpdate, Item>()
+            .ForMember(s => s.ItemId, opt => opt.MapFrom(s => s.Id))
+           .ForMember(s => s.ItemName, opt => opt.MapFrom(s => s.Name))
+           .ForMember(s => s.ItemPrice, opt => opt.MapFrom(s => s.Price))
+           .ForMember(s => s.ItemDescription, opt => opt.MapFrom(s => s.Description));
+        }
+    }
+}
