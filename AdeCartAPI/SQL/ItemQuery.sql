3d74bfd08239dd1321293f4279062d72ab490656
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-06 09:58:11 +0100

    Query for item model

---
+++ AdeCartAPI/SQL/ItemQuery.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Item](
+ [ItemId] int IDENTITY(1,1) NOT NULL,
+ [ItemName] nchar(100) NOT NULL,
+ [ItemPrice] smallmoney NOT NULL,
+ [ItemDescription] nchar(300) NOT NULL,
+ [AvailableItem] tinyint NOT NULL,
+ CONSTRAINT [PK_Item] PRIMARY KEY CLUSTERED ([ItemId] ASC)
+);
