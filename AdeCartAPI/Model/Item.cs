b3ee3b33a788555de42468b804bebdc0cdcae2c7
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-06 09:57:34 +0100

    Created Item Model Class

---
+++ AdeCartAPI/Model/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdeCartAPI.Model
+{
+    public class Item
+    {
+        [Key]
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public int ItemPrice { get; set; }
+        public string Description { get; set; }
+        public int AvailableItem { get; set; }
+    }
+}
