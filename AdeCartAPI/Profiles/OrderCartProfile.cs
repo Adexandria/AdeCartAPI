e931a3efb931f7e136ac3cadaaaf36c17cdf07b0
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-08 13:38:58 +0100

    Created Profile for ordercart

---
+++ AdeCartAPI/Profiles/OrderCartProfile.cs
@@ -0,0 +1,21 @@
+﻿using AdeCartAPI.DTO;
+using AdeCartAPI.Model;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdeCartAPI.Profiles
+{
+    public class OrderCartProfile :Profile
+    {
+        public OrderCartProfile()
+        {
+            CreateMap<OrderCartData, OrderCart>();
+            CreateMap<OrderCart, OrderCartDTO>().ForMember(s=>s.OrderStatus,map=>map.MapFrom(s=>s.OrderStatus.ToString()));
+            CreateMap<OrderCart, OrderCartsDTO>().ForMember(s => s.OrderStatus, map => map.MapFrom(s => s.OrderStatus.ToString()));
+            CreateMap<CartUpdate, OrderCart>();
+        }
+    }
+}
