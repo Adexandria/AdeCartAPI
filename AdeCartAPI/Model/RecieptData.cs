e19ae159b229937ea4894d232c719e51aa1aebd5
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-09 15:31:15 +0100

    added staus property

--- AdeCartAPI/Model/RecieptData.cs
+++ AdeCartAPI/Model/RecieptData.cs
@@ -10,6 +10,8 @@ namespace AdeCartAPI.Model
         public string Amount { get; set; }
         [JsonProperty("message")]
         public string Message { get; set; }
+        [JsonProperty("Status")]
+        public string Status { get; set; }
         [JsonProperty("gateway_response")]
         public string Response { get; set; }
     }
