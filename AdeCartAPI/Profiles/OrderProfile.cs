080fcd8079aa2c6b261dbdfbf02e144b04de0a6e
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-09 10:26:03 +0100

    Created a profile for Order model

---
+++ AdeCartAPI/Profiles/OrderProfile.cs
@@ -0,0 +1,20 @@
+﻿using AdeCartAPI.DTO;
+using AdeCartAPI.Model;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdeCartAPI.Profiles
+{
+    public class OrderProfile :Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Order, OrderDTO>();
+            CreateMap<OrderUpdate, Order>();
+            CreateMap<OrderCreate, Order>();
+        }
+    }
+}
