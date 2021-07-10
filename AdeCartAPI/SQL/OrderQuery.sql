5309ba8baaadf77863e7f9e7caae7245f45ce276
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-10 06:44:09 +0100

    Added Quantity field

--- AdeCartAPI/SQL/OrderQuery.sql
+++ AdeCartAPI/SQL/OrderQuery.sql
@@ -2,6 +2,7 @@
 [OrderId] int IDENTITY(1,1) NOT NULL,
 [ItemId] int NOT NULL,
 [OrderCartId] int  NOT NULL,
+[Quantity]    INT NOT NULL,
 CONSTRAINT [PK_Order] PRIMARY KEY CLUSTERED ([OrderId] ASC),
 CONSTRAINT [FK_Order_Item_ItemId] FOREIGN KEY ([ItemId]) REFERENCES [dbo].[Item] ([ItemId]) ON DELETE CASCADE,
 CONSTRAINT [FK_Order_OrderCart_OrderCartId] FOREIGN KEY ([OrderCartId]) REFERENCES [dbo].[OrderCart] ([OrderCartId]) ON DELETE CASCADE
