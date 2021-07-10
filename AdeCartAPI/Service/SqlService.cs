52af591da453dbb2a06a216d5416ab24532f28ac
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-10 06:10:35 +0100

    Created a class to call sql connection using the connection string

---
+++ AdeCartAPI/Service/SqlService.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+namespace AdeCartAPI.Service
+{
+    public class SqlService
+    {
+        string connectionString;
+        readonly IConfiguration _config;
+
+        public SqlService(IConfiguration _config)
+        {
+            this._config = _config;
+        }
+
+        public SqlConnection CreateConnection()
+        {
+            connectionString = _config["ConnectionStrings:AdeCart"];
+            var sqlConnection = new SqlConnection(connectionString);
+            return sqlConnection;
+        }
+    }
+}
