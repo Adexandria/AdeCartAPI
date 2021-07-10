344728f2959ee7970ffe5e657258ea8dd8fd49e6
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-10 06:44:29 +0100

    Removed addressbox2 field

--- AdeCartAPI/SQL/UserAddressQuery.sql
+++ AdeCartAPI/SQL/UserAddressQuery.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [dbo].[UserAddress](
  [AddressId] INT IDENTITY(1,1) NOT NULL,
- [AddressBox1] NVARCHAR(20) NOT NULL,
- [AddressBox2] NVARCHAR(20) NULL,
+ [AddressBox1] NVARCHAR(120) NOT NULL,
  [UserId] NVARCHAR (450) NOT NULL
  CONSTRAINT [PK_UserAddress] PRIMARY KEY CLUSTERED ([AddressId] ASC),
  CONSTRAINT [FK_UserAddress_AspNetUsers_Id] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
