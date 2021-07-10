a7e5876c47229c65b2751c463bcb68bc696736ef
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-10 07:06:37 +0100

    Clean code

--- AdeCartAPI/UserModel/UserAddress.cs
+++ AdeCartAPI/UserModel/UserAddress.cs
@@ -1,10 +1,6 @@
 ﻿using AdeCartAPI.Model;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AdeCartAPI.UserModel
 {
