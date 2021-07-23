c824eaea5a7dcc1e81902b8afecfd4510021576e
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-23 11:16:27 +0100

    the login method generates jwt

--- AdeCartAPI/Controllers/UserController.cs
+++ AdeCartAPI/Controllers/UserController.cs
@@ -10,13 +10,15 @@ using AdeCartAPI.Model;
 using AdeCartAPI.UserModel;
 using System.Net;
 using Swashbuckle.AspNetCore.Annotations;
+using AdeCartAPI.Service;
+using System.IdentityModel.Tokens.Jwt;
+using System;
 
 namespace AdeCartAPI.Controllers
 {
     [SwaggerResponse((int)HttpStatusCode.OK, "Returns if sucessful")]
     [SwaggerResponse((int)HttpStatusCode.NotFound, "Returns if not found")]
     [SwaggerResponse((int)HttpStatusCode.BadRequest, "Returns no content")]
-    [SwaggerResponse((int)HttpStatusCode.Unauthorized)]
 
     [ApiController]
     [Route("api/User")]
@@ -26,15 +28,17 @@ namespace AdeCartAPI.Controllers
         readonly UserManager<User> user;
         private readonly SignInManager<User> login;
         private readonly IPasswordHasher<User> passwordHasher;
+        private Credentials _credentials;
 
         readonly IMapper mapper;
 
-        public UserController(UserManager<User> user, IMapper mapper, SignInManager<User> login, IPasswordHasher<User> passwordHasher)
+        public UserController(UserManager<User> user, IMapper mapper, SignInManager<User> login, IPasswordHasher<User> passwordHasher, Credentials _credentials)
         {
             this.user = user;
             this.mapper = mapper;
             this.login = login;
             this.passwordHasher = passwordHasher;
+            this._credentials = _credentials;
         }
         ///<param name="newuser">
         ///an object to sign up a user
@@ -50,24 +54,31 @@ namespace AdeCartAPI.Controllers
         [HttpPost("signup")]
         public async Task<ActionResult> SignUp(SignUp newuser)
         {
-            var signup = mapper.Map<User>(newuser);
-            if (newuser.Password.Equals(newuser.RetypePassword))
+            try
             {
-                IdentityResult identity = await user.CreateAsync(signup, signup.PasswordHash);
+                var signup = mapper.Map<User>(newuser);
+                if (newuser.Password.Equals(newuser.RetypePassword))
+                {
+                    IdentityResult identity = await user.CreateAsync(signup, signup.PasswordHash);
 
-                if (identity.Succeeded)
-                {
-                    await user.AddClaimAsync(signup, new Claim(ClaimTypes.Role, "User"));
-                    await login.SignInAsync(signup, false);
-                    var token = await EmailConfirmationToken(signup);
-                    return this.StatusCode(StatusCodes.Status201Created, $"Welcome,{signup.UserName} use this {token} verify email");
+                    if (identity.Succeeded)
+                    {
+                        var token = await EmailConfirmationToken(signup);
+                        return this.StatusCode(StatusCodes.Status201Created, $"Welcome,{signup.UserName} use this {token} verify email");
+                    }
+                    else
+                    {
+                        return BadRequest("The username exists or check password requirements");
+                    }
                 }
-                else
-                {
-                    return BadRequest("The username exists or check password requirements");
-                }
+                return this.StatusCode(StatusCodes.Status400BadRequest, "Password not equal,retype password");
             }
-            return this.StatusCode(StatusCodes.Status400BadRequest, "Password not equal,retype password");
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+            
         }
         ///<param name="username">
         ///\a user's username
@@ -83,17 +94,26 @@ namespace AdeCartAPI.Controllers
         [HttpPost("{username}/emailconfirmation")]
         public async Task<ActionResult> VerifyEmailToken(Token token,string username) 
         {
-            var currentUser = await GetUser(username);
-            if (currentUser == null) return NotFound("username doesn't exist");
-            var result = await user.ConfirmEmailAsync(currentUser, token.GeneratedToken);
-            if (result.Succeeded) 
+            try
             {
-                return this.StatusCode(StatusCodes.Status200OK, $"Welcome,{currentUser.UserName} Email has been verified");
+                var currentUser = await GetUser(username);
+                if (currentUser == null) return NotFound("username doesn't exist");
+                var result = await user.ConfirmEmailAsync(currentUser, token.GeneratedToken);
+                if (result.Succeeded)
+                {
+                    return this.StatusCode(StatusCodes.Status200OK, $"Welcome,{currentUser.UserName} Email has been verified");
+                }
+                else
+                {
+                    return this.StatusCode(StatusCodes.Status400BadRequest, "Invalid Token");
+                }
             }
-            else
+            catch (Exception e)
             {
-                return this.StatusCode(StatusCodes.Status400BadRequest, "Invalid Token");
+
+                return BadRequest(e.Message);
             }
+          
         }
         ///<param name="model">
         ///an object to login a user
@@ -108,20 +128,33 @@ namespace AdeCartAPI.Controllers
         [HttpPost("login")]
         public async Task<ActionResult> Login(Login model)
         {
-            var logindetails = mapper.Map<User>(model);
-            var currentUser = await user.FindByNameAsync(logindetails.UserName);
-            if (currentUser == null) return NotFound("Username doesn't exist");
-            //This verfies the user password by using IPasswordHasher interface
-            var passwordVerifyResult = passwordHasher.VerifyHashedPassword(currentUser, currentUser.PasswordHash, model.Password);
-            if (passwordVerifyResult.ToString() == "Success")
+            try
             {
-                await login.SignInAsync(currentUser, false);
-                await login.CreateUserPrincipalAsync(currentUser);
+                var logindetails = mapper.Map<User>(model);
+                var currentUser = await user.FindByNameAsync(logindetails.UserName);
+                if (currentUser == null) return NotFound("Username doesn't exist");
+                //This verfies the user password by using IPasswordHasher interface
+                var passwordVerifyResult = passwordHasher.VerifyHashedPassword(currentUser, currentUser.PasswordHash, model.Password);
+                if (passwordVerifyResult.ToString() == "Success")
+                {
+                   // await login.SignInAsync(currentUser, false);
+                    var signingCredentials = _credentials.GetSigningCredentials();
+                    var claims = await _credentials.GetClaims(currentUser);
+                    var tokenOptions = _credentials.GenerateTokenOptions(signingCredentials,claims);
+                    var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+                    return Ok(token);
+                    /* 
+                     await login.CreateUserPrincipalAsync(currentUser);return this.StatusCode(StatusCodes.Status200OK, $"Welcome,{currentUser.UserName} Shop Shop Shop");*/
+                }
 
-                return this.StatusCode(StatusCodes.Status200OK, $"Welcome,{currentUser.UserName} Shop Shop Shop");
+                return BadRequest("password is not correct");
             }
+            catch (Exception e)
+            {
 
-            return BadRequest("password is not correct");
+                return BadRequest(e.Message);
+            }
+           
         }
 
         ///<param name="username">
@@ -137,10 +170,19 @@ namespace AdeCartAPI.Controllers
         [HttpGet("{username}/password/reset")]
         public async Task<ActionResult> ResetPassword(string username) 
         {
-            var currentUser = await GetUser(username);
-            if (currentUser == null) return NotFound("username doesn't exist");
-            var passwordResetToken = await user.GeneratePasswordResetTokenAsync(currentUser);
-            return Ok($"Reset Password Token {passwordResetToken}");
+            try
+            {
+                var currentUser = await GetUser(username);
+                if (currentUser == null) return NotFound("username doesn't exist");
+                var passwordResetToken = await user.GeneratePasswordResetTokenAsync(currentUser);
+                return Ok($"Reset Password Token {passwordResetToken}");
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+           
         }
         ///<param name="username">
         ///\a user's username
@@ -158,19 +200,28 @@ namespace AdeCartAPI.Controllers
         [HttpPost("{username}/password/verifytoken")]
         public async Task<ActionResult> VerifyPasswordToken(ResetPassword resetPassword,string username) 
         {
-            var currentUser = await GetUser(username);
-            if (currentUser == null) return NotFound("username doesn't exist");
-            var isVerify = passwordHasher.VerifyHashedPassword(currentUser, currentUser.PasswordHash, resetPassword.NewPassword);
-            if (isVerify.ToString() == "Success") return BadRequest("Old password can't be new password");
-            var isVerifyResult = await user.ResetPasswordAsync(currentUser, resetPassword.Token, resetPassword.NewPassword);
-            if (isVerifyResult.Succeeded)
+            try
             {
-                return Ok("Password changed");
+                var currentUser = await GetUser(username);
+                if (currentUser == null) return NotFound("username doesn't exist");
+                var isVerify = passwordHasher.VerifyHashedPassword(currentUser, currentUser.PasswordHash, resetPassword.NewPassword);
+                if (isVerify.ToString() == "Success") return BadRequest("Old password can't be new password");
+                var isVerifyResult = await user.ResetPasswordAsync(currentUser, resetPassword.Token, resetPassword.NewPassword);
+                if (isVerifyResult.Succeeded)
+                {
+                    return Ok("Password changed");
+                }
+                else
+                {
+                    return BadRequest(isVerifyResult.Errors);
+                }
             }
-            else
+            catch (Exception e)
             {
-                return BadRequest(isVerifyResult.Errors);
+
+                return BadRequest(e.Message);
             }
+           
         }
         ///<param name="username">
         ///\a user's username
@@ -186,17 +237,26 @@ namespace AdeCartAPI.Controllers
         [HttpPost("{username}/phonenumber/add")]
         public async Task<ActionResult<User>> AddPhoneNumber(UserNumber phoneNumber,string username) 
         {
-            var currentUser = await GetUser(username);
-            if (currentUser == null) return NotFound("username doesn't exist");
-            var result = await user.SetPhoneNumberAsync(currentUser, phoneNumber.PhoneNumber);
-            if (result.Succeeded) 
+            try
             {
-                return this.StatusCode(StatusCodes.Status200OK, "Successfully Changed");
+                var currentUser = await GetUser(username);
+                if (currentUser == null) return NotFound("username doesn't exist");
+                var result = await user.SetPhoneNumberAsync(currentUser, phoneNumber.PhoneNumber);
+                if (result.Succeeded)
+                {
+                    return this.StatusCode(StatusCodes.Status200OK, "Successfully Changed");
+                }
+                else
+                {
+                    return BadRequest(result.Errors);
+                }
             }
-            else 
+            catch (Exception e)
             {
-                return BadRequest(result.Errors);
+
+                return BadRequest(e.Message); 
             }
+            
         }
         ///<param name="username">
         ///\a user's username
@@ -212,18 +272,27 @@ namespace AdeCartAPI.Controllers
         [HttpGet("{username}/phonenumber/change")]
         public async Task<ActionResult<User>> ChangePhonenumber(string username,UserNumber phoneNumber) 
         {
-            var currentUser = await GetUser(username);
-            if (currentUser == null) return NotFound("username doesn't exist");
-            var token = await user.GenerateChangePhoneNumberTokenAsync(currentUser, phoneNumber.PhoneNumber);
-            var result = await user.ChangePhoneNumberAsync(currentUser, phoneNumber.PhoneNumber, token);
-            if (result.Succeeded)
+            try
             {
-                return this.StatusCode(StatusCodes.Status200OK, "Successfully Changed");
+                var currentUser = await GetUser(username);
+                if (currentUser == null) return NotFound("username doesn't exist");
+                var token = await user.GenerateChangePhoneNumberTokenAsync(currentUser, phoneNumber.PhoneNumber);
+                var result = await user.ChangePhoneNumberAsync(currentUser, phoneNumber.PhoneNumber, token);
+                if (result.Succeeded)
+                {
+                    return this.StatusCode(StatusCodes.Status200OK, "Successfully Changed");
+                }
+                else
+                {
+                    return BadRequest(result.Errors);
+                }
             }
-            else
+            catch (Exception e)
             {
-                return BadRequest(result.Errors);
+
+                return BadRequest(e.Message);
             }
+           
         }
         ///<param name="username">
         ///\a user's username
@@ -239,17 +308,26 @@ namespace AdeCartAPI.Controllers
         [HttpPost("{username}/change")]
         public async Task<ActionResult<User>> ChangeUsername(UserName name,string username) 
         {
-            var currentUser = await GetUser(username);
-            if (currentUser == null) return NotFound("username doesn't exist");
-            var result = await user.SetUserNameAsync(currentUser,name.Username);
-            if (result.Succeeded)
+            try
             {
-                return this.StatusCode(StatusCodes.Status200OK, "Successfully Changed");
+                var currentUser = await GetUser(username);
+                if (currentUser == null) return NotFound("username doesn't exist");
+                var result = await user.SetUserNameAsync(currentUser, name.Username);
+                if (result.Succeeded)
+                {
+                    return this.StatusCode(StatusCodes.Status200OK, "Successfully Changed");
+                }
+                else
+                {
+                    return BadRequest(result.Errors);
+                }
             }
-            else
+            catch (Exception e)
             {
-                return BadRequest(result.Errors);
+
+                return BadRequest(e.Message);
             }
+           
         }
         ///<param name="username">
         ///\a user's username
@@ -263,10 +341,19 @@ namespace AdeCartAPI.Controllers
         [HttpPost("{username}/signout")]
         public async Task<ActionResult> Signout(string username)
         {
-            var currentUser = await GetUser(username);
-            if (currentUser == null) return NotFound ("username doesn't exist");
-            await login.SignOutAsync();
-            return Ok();
+            try
+            {
+                var currentUser = await GetUser(username);
+                if (currentUser == null) return NotFound("username doesn't exist");
+                await login.SignOutAsync();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+            
         }
 
         [NonAction]
@@ -283,3 +370,4 @@ namespace AdeCartAPI.Controllers
       
     }
 }
+ 
